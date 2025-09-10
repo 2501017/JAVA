2124aa5594bb9b5960c156d77a5bf15a5215f1d8
jawon <jawon1117@gmail.com>
2025-09-10 15:35:06 +0900

    2 주차 수업자료

---
+++ week02/Sample2/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        //준비물
+        Scanner keyboard = new Scanner(System.in);
+        String name = "";
+        //data 입력
+        System.out.print("당신의 이름은 ?");
+        name = keyboard.nextLine();
+        //처리
+
+        //information 출력(필수)
+        System.out.println("이름 : " + name);
+
+
+
+
+
+    }
+}
---
+++ week02/Sample3/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        //준비물
+        int first = 0;
+        int second = 0;
+        int result = 0;
+        Scanner keyboard = new Scanner(System.in);
+        //data 입력
+        System.out.print("첫 번째 값 입력 :");
+        first = keyboard.nextInt();
+        System.out.print("두 번째 값 입력 :");
+        second = keyboard.nextInt();
+
+        //처리
+        result = first * second;
+
+        //출력
+        System.out.printf("%,d * %,d = %,d\n",first, second, result);
+
+
+    }
+}
