bed14b8058f799bc82b05a05791613f975a41064
jawon <jawon1117@gmail.com>
2025-10-01 12:44:19 +0900

    2주차와 3주차 리포트

--- report3/report3-1/src/Main.java
+++ report3/report3-1/src/Main.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+        Scanner keyboard = new Scanner(System.in);
+        String name = "홍길동";
+        String school ="경복대학교";
+        int age = 20;
+        char gender =' ';
+        float height = 170.6f;
+        float weight = 65.4f;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
-        }
+        System.out.print("학교 : ");
+        school = keyboard.nextLine();
+        System.out.print("이름 : ");
+        name = keyboard.nextLine();
+        System.out.print("성별 : ");
+        gender = keyboard.next().charAt(0);
+        System.out.print("신장 : ");
+        height = keyboard.nextFloat();
+        System.out.print("체중 : ");
+        weight = keyboard.nextFloat();
+
+
+
+        System.out.printf("학교 : %s\n",school);
+        System.out.printf("이름 : %s\n",name);
+        System.out.printf("나이 : %d\n",age);
+        System.out.printf("성별 : %s\n",gender);
+        System.out.printf("신장 : %.1f\n",height);
+        System.out.printf("체중 : %.1f\n",weight);
     }
 }
